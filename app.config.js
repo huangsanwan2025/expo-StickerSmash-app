b48840e57b4d41625f7c97222967ef58ab7c1a8d
xiaobu <864181495@qq.com>
2026-01-29 10:45:11 +0800

    feat: 构建配置的更改

---
+++ app.config.js
@@ -0,0 +1,46 @@
+const IS_DEV = process.env.APP_VARIANT === 'development';
+const IS_PREVIEW = process.env.APP_VARIANT === 'preview';
+
+const getUniqueIdentifier = () => {
+  if (IS_DEV) {
+    return 'com.hlb.stickersmash.dev';
+  }
+
+  if (IS_PREVIEW) {
+    return 'com.hlb.stickersmash.preview';
+  }
+
+  return 'com.hlb.stickersmash';
+};
+
+const getAppName = () => {
+  if (IS_DEV) {
+    return 'StickerSmash (Dev)';
+  }
+
+  if (IS_PREVIEW) {
+    return 'StickerSmash (Preview)';
+  }
+
+  return 'StickerSmash: Emoji Stickers';
+};
+
+
+export default ({ config }) => {
+  console.log('--config--', config)
+
+  return ({
+  ...config,
+  name: getAppName(),
+  ios: {
+    ...config.ios,
+    buildNumber: '1',
+    bundleIdentifier: getUniqueIdentifier(),
+  },
+  android: {
+    ...config.android,
+    versionCode: '1',
+    package: getUniqueIdentifier(),
+  },
+})
+};
